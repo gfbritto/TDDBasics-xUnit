5cb92dde48ceabd05ceba42432200e4d439766cb
Gabriel Brito <gabriel.brito@take.net>
2022-01-06 21:23:31 -0300

    Add project files.

---
+++ GameEngine.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EnemyFactoryShould
+    {
+        [Fact]
+        public void CreateNormalEnemyByDefault()
+        {
+            var sut = new EnemyFactory();
+
+            var enemy = sut.Create("Peter");
+
+            Assert.IsType<NormalEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy()
+        {
+            var sut = new EnemyFactory();
+
+            var enemy = sut.Create("Peter King", true);
+
+            Assert.IsType<BossEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_CastReturnType()
+        {
+            var sut = new EnemyFactory();
+
+            var enemy = sut.Create("Peter King", true);
+
+            var boss = Assert.IsType<BossEnemy>(enemy);
+
+            Assert.Equal("Peter King", boss.Name);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_IsAssignable()
+        {
+            var sut = new EnemyFactory();
+
+            var enemy = sut.Create("Peter King", true);
+
+            Assert.IsAssignableFrom<Enemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateSeparedInstances()
+        {
+            var sut = new EnemyFactory();
+
+            var enemy1 = sut.Create("Peter");
+            var enemy2 = sut.Create("Peter");
+
+            Assert.NotSame(enemy1,enemy2);
+        }
+    }
+}
---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,142 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.True(sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateCharacterFullName()
+        {
+            var sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Connor";
+
+            Assert.Equal("Sarah Connor", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameStartingWithFirstName()
+        {
+            var sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Connor";
+
+            Assert.StartsWith("Sarah", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameEndingWithFirstName()
+        {
+            var sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Connor";
+
+            Assert.EndsWith("COnnor", sut.FullName, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void CalculateFullNameWithTitleCase()
+        {
+            var sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Connor";
+
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.Equal(100, sut.Health);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth_NotEqual()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.NotEqual(0, sut.Health);
+        }
+
+        [Fact]
+        public void IncreaseHealthAfterSleep()
+        {
+            var sut = new PlayerCharacter();
+
+            sut.Sleep();
+
+            Assert.InRange(sut.Health, 101, 200);
+        }
+
+        [Fact]
+        public void NotHaveNickNameByDefault()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.Null(sut.Nickname);
+        }
+
+        [Fact]
+        public void HaveDefaultWeapons()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.Contains("Long Bow", sut.Weapons);
+        }
+
+        [Fact]
+        public void NotHaveAStaffOfWonder()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.DoesNotContain("Staff Of Wonder", sut.Weapons);
+        }
+
+        [Fact]
+        public void HaveSomeKindOfSword()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.Contains(sut.Weapons, weapon => weapon.Contains("Sword"));
+        }
+
+        [Fact]
+        public void HaveAllExpectedWeapons()
+        {
+            var sut = new PlayerCharacter();
+
+            var weapons = new[]
+            {
+                "Long Bow",
+                "Short Bow",
+                "Short Sword"
+            };
+
+            Assert.Equal(weapons, sut.Weapons);
+        }
+
+        [Fact]
+        public void HaveNoemptyDefaultWeapons()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.All(sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));
+        }
+    }
+}
