30a4adf058acd336a9df4ebdd27d1b3a28836790
Gabriel Brito <gabriel.brito@take.net>
2022-01-16 22:23:04 -0300

    add external data for tets

---
+++ GameEngine.Tests/Data/ExternalHealthDamageTestData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GameEngine.Tests.Data
+{
+    public static class ExternalHealthDamageTestData
+    {
+        private const string Path = "Data/TestData.csv";
+
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                var csvLines = File.ReadAllLines(Path);
+
+                var testCases = new List<object[]>();
+
+                foreach (var csvLine in csvLines)
+                {
+                    IEnumerable<int> values = csvLine.Split(',').Select(int.Parse);
+
+                    object[] testCase = values.Cast<object>().ToArray();
+
+                    testCases.Add(testCase);
+                }
+                return testCases;
+            }
+        }
+    }
+}
--- GameEngine.Tests/NonPlayerCharacterShould.cs
+++ GameEngine.Tests/NonPlayerCharacterShould.cs
@@ -7,7 +7,8 @@ namespace GameEngine.Tests
     public class NonPlayerCharacterShould
     {
         [Theory]
-        [MemberData(nameof(InternalHealthDamageTestData.TestData), MemberType = typeof(InternalHealthDamageTestData))]
+        //[MemberData(nameof(ExternalHealthDamageTestData.TestData), MemberType = typeof(ExternalHealthDamageTestData))]
+        [HealthDamageData]
         public void TakeDamage(int damage, int expectedHealth)
         {
             NonPlayerCharacter sut = new NonPlayerCharacter();
